e3e488c098f8c9ad5c577621b9a937476dc11d40
Matheus <theuszero@gmail.com>
2025-07-18 17:19:09 -0300

    Adicionando testes de transferência

--- test/login.test.js
+++ test/login.test.js
@@ -1,19 +1,19 @@
 const request = require('supertest')
 const { expect } = require("chai")
 
-    describe('Login', () => {
-        describe('POST/login', () => {
-            it('Deve retornar 200 com um token em string quando usar credenciais válidas', async () => {
-                const response = await request('http://localhost:3000')
+describe('Login', () => {
+    describe('POST/login', () => {
+        it('Deve retornar 200 com um token em string quando usar credenciais válidas', async () => {
+            const response = await request('http://localhost:3000')
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send({
                         'username': 'julio.lima',
                         'senha': '123456'
                     })
-                expect(response.status).to.equal(200)
-                expect(response.body.token).to.be.a('string')
 
-            })
+            expect(response.status).to.equal(200)
+            expect(response.body.token).to.be.a('string')
         })
     })
+})
