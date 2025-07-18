24b1fa4596c9341fcfd298fa886c320a0fa14778
Matheus <theuszero@gmail.com>
2025-07-18 17:35:20 -0300

    adicionando a biblioteca .env para variáveis globais

--- test/transferencias.test.js
+++ test/transferencias.test.js
@@ -1,10 +1,12 @@
 const request = require('supertest')
 const { expect } = require("chai")
+require('dotenv').config()
 
 describe('Transfêrencias', () => {
     describe('POST/transferencias', () => {
         it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$10,00', async () => {
-            const responseLogin = await request('http://localhost:3000')
+            const responseLogin = await request(process.env.BASE_URL)
+            
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send({
@@ -14,7 +16,7 @@ describe('Transfêrencias', () => {
 
             const token = responseLogin.body.token
 
-            const response = await request('http://localhost:3000')
+            const response = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
@@ -28,7 +30,7 @@ describe('Transfêrencias', () => {
         })
 
         it('Deve retornar falha com 422 quando o valor da transferência for abaixo de R$10,00', async () => {
-            const responseLogin = await request('http://localhost:3000')
+            const responseLogin = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send({
@@ -38,7 +40,7 @@ describe('Transfêrencias', () => {
 
             const token = responseLogin.body.token
 
-            const response = await request('http://localhost:3000')
+            const response = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
